a8bb411390abe7081008bf3abb014f2834734559
Thomas Bonnefille <thomas.bonnefille@bootlin.com>
2025-10-19 13:10:23 +0200

    Add db
    Signed-off-by: Thomas Bonnefille <thomas.bonnefille@bootlin.com>

--- cleaner.py
+++ cleaner.py
@@ -1,12 +1,16 @@
-import re
+import re, os
 
 
 class Cleaner:
-    def __init__(self, filename=None):
+    def __init__(self, path=None):
         self.name_history = []
         self.mediatype = None
-        self.filename = filename
         self.date = 1900
+        self.path = path
+        try:
+            self.filename = os.path.basename(path)
+        except TypeError:
+            self.filename = ""
 
     def clean_extension(self, orig: str):
         self.filename = orig.rsplit('.', 1)[0]
@@ -21,25 +25,32 @@ class Cleaner:
             # Extract everything before the year
             title_part = name[:match.start()]
             self.date = int(match.group(0))
+            print(f"Date : {self.date}")
         else:
             # If no year is found, take the whole name
             title_part = name
+            self.date = 1900
+            print(f"no date {self.date}")
 
         self.filename = title_part
         return title_part
 
     def replace_dot_with_space(self, orig: str):
         self.filename = orig.replace('.', ' ').strip()
+        self.filename = self.filename.replace('_', ' ').strip()
         return self.filename
 
     def remove_site(self, orig: str):
         name = orig
         name = re.sub(r'^\[.*?\]\s*', '', name)
+        if (name[-1] in ['(', '[']):
+            name = name[:-1]
         self.filename = name
         return name
 
-    def full_clean(self, orig: str):
-        self.filename = orig
+    def full_clean(self, path: str):
+        self.path = path
+        self.filename = os.path.basename(path)
         self.clean_extension(self.filename)
         self.clean_from_date(self.filename)
         self.remove_site(self.filename)
---
+++ mdb.py
@@ -0,0 +1,81 @@
+import sqlite3
+import tmdbv3api
+from secret import SECRET_API_KEY
+import os
+import shutil
+from cleaner import Cleaner
+import wget
+
+class Movie:
+    def __init__(self):
+        self.name = ""
+        self.path = ""
+        self.date = 1900
+        self.desc = ""
+        self.tmdbid = 0
+
+class MovieDB:
+    def __init__(self):
+        self.con = sqlite3.connect("movies.db")
+        self.cur = self.con.cursor()
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS movies(id INTEGER PRIMARY KEY AUTOINCREMENT, name, description, year, path, tmdbid)")
+        self.con.commit()
+        self.tmdb = tmdbv3api.TMDb()
+        self.tmdb.api_key = SECRET_API_KEY
+        self.tmdb.language = "fr"
+        self.movie_tmbd = tmdbv3api.Movie()
+
+    def create_new_entry(self, movie: Movie):
+        self.cur.execute(
+            "INSERT OR IGNORE INTO movies (name, description, year, path, tmdbid) VALUES (?,?,?,?,?)",
+            (movie.name, movie.desc, movie.date, movie.path, movie.tmdbid))
+        self.con.commit()
+        return self.cur.lastrowid
+
+    def tmdb_search(self, cleaner: Cleaner):
+        search = self.movie_tmbd.search(cleaner.filename)
+        movie = Movie()
+        movie.date = cleaner.date
+        if (search.total_results == 0):
+            movie.name = cleaner.filename
+            movie.desc = "Unknown"
+            movie.date = 1900
+            movie.path = cleaner.path
+            movie.tmdbid = 0
+            id = self.create_new_entry(movie)
+            shutil.copy("static/pictures/none.jpg",
+                        "static/pictures/"+str(id)+".jpg")
+            return movie
+
+
+        if (cleaner.date == 1900):
+            print("Clearly no date")
+            good_res = search[0]
+        else:
+            # Find the nearest movie
+            smallest_gap = 100
+            for res in search:
+                try:
+                    year = int(res.release_date[:4])
+                    if abs(year - movie.date) < smallest_gap:
+                        smallest_gap = abs(year - movie.date)
+                        print(f"{year} - {movie.date}")
+                        good_res = res
+                except:
+                    pass
+
+        print(good_res)
+        movie.name = good_res.title
+        movie.desc = good_res.overview
+        movie.date = good_res.release_date
+        movie.path = cleaner.path
+        movie.tmdbid = good_res.id
+        id = self.create_new_entry(movie)
+        self.download_picture(good_res.poster_path, id)
+
+    def download_picture(self, filename: str, id):
+        url = "https://image.tmdb.org/t/p/w1280"+ filename
+        filename = wget.download(url)
+
+        os.rename(filename, "static/pictures/"+str(id)+".jpg")
